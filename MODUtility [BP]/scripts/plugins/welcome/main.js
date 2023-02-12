867caa7b8717c0bf877873ea2eb62d0299f098f4
ModdedPro14 <101987712+ModdedPro14@users.noreply.github.com>
2023-02-12 09:48:32 -0800

    Delete MODUtility [BP] directory

--- MODUtility [BP]/scripts/plugins/welcome/main.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-
-world.events.playerSpawn.subscribe(data => {
-    const player = data.player
-    if (!player.hasTag('old')) {
-        world.say(`§c${player.name}§r§7 has connected for the first time. Please welcome them`)
-        player.addTag('old')
-    }
-});
